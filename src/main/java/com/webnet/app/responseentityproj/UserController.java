3cdbeb9c5b22a5448959fceafdf11eef292a8b7e
abammeke <abammeke222@gmail.com>
2023-05-25 20:31:11 -0500

    modify edited method

--- src/main/java/com/webnet/app/responseentityproj/UserController.java
+++ src/main/java/com/webnet/app/responseentityproj/UserController.java
@@ -56,7 +56,7 @@ public class UserController implements CoreController {
     }
 
     @GetMapping("/books/4")
-    public ResponseEntity<Response<UserResponse>> getBookFourEdited(){
+    public ResponseEntity<Response<UserResponse>> getBookFourEdit(){
         UserResponse userResponse = new UserResponse();;
         userResponse.setBooks(Book.getBooks());
         return invokeService( () -> {
